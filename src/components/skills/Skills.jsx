1ab699daeb6fd83266a661ea1377e961b3467b4b
Diego Arthur <digo.arthur@hotmail.com>
2022-06-30 13:58:49 +0100

    add build skills structure, elements, effect on component and button

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,126 @@
+import './styles.scss'
+import { IoIosArrowDown } from "react-icons/io";
+
+export function Skills() {
+
+    const style = { color: "white", fontSize: "1.5em" }
+    
+    return (
+        <div className="skillsContainer">
+            <h1>Main Stack</h1>
+
+            <section className="typeSkillsContainer">
+                <h5>Tools</h5>
+                <div className="itemsContainer">
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                </div>
+            </section>
+
+            <section className="typeSkillsContainer">
+                <h5>Languages | Frameworks</h5>
+                <div className="itemsContainer">
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                </div>
+            </section>
+
+
+            <section className="typeSkillsContainer">
+
+                <h5>Databases</h5>
+                <div className="itemsContainer">
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                    <div className="itemsContent">
+                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
+                        <span>NodeJS</span>
+                    </div>
+                </div>
+            </section>
+
+            <footer>
+                
+                <button> Projetos  <IoIosArrowDown className="iconButton" style={{}}/></button>
+            </footer>
+
+        </div>
+    )
+}
