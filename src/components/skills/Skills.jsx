ca003fd34fc4810a1e87b5efdc549e099f72aa76
Diego Arthur <digo.arthur@hotmail.com>
2022-07-14 22:32:23 +0100

    add skills database MainStack

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,10 @@
 import './styles.scss'
 import { IoIosArrowDown } from "react-icons/io";
-
+import skillsData from '../../data/skills_data.json'
 export function Skills() {
 
     const style = { color: "white", fontSize: "1.5em" }
-    
+
     return (
         <div className="skillsContainer">
             <h1>Main Stack</h1>
@@ -12,74 +12,33 @@ export function Skills() {
             <section className="typeSkillsContainer">
                 <h5>Tools</h5>
                 <div className="itemsContainer">
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
+                    {
+                        skillsData.Tools.map((item) => (
+                            <div className="itemsContent">
+                                <img className='codeBar' src={item.logo}></img>
+                                <span>{item.name}</span>
+                            </div>
 
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
+                        ))
+
+                    }
                 </div>
             </section>
 
             <section className="typeSkillsContainer">
                 <h5>Languages | Frameworks</h5>
                 <div className="itemsContainer">
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
+                    {
+                        skillsData.LanguagesFrameworks.map(item => (
+                            <div className="itemsContent">
+                                <img className='codeBar' src={item.logo}></img>
+                                <span>{item.name}</span>
+                            </div>
 
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
+                        ))
+                    }
+
+
                 </div>
             </section>
 
@@ -88,39 +47,24 @@ export function Skills() {
 
                 <h5>Databases</h5>
                 <div className="itemsContainer">
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
 
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
-                    <div className="itemsContent">
-                        <img className='codeBar' src="https://user-images.githubusercontent.com/59892368/176472287-7918843b-9aa8-45f7-833a-b21fcd5a3a2c.svg"></img>
-                        <span>NodeJS</span>
-                    </div>
+                    {
+                        skillsData.Databases.map(item => (
+                            <div className="itemsContent">
+                            <img className='codeBar' src={item.logo}></img>
+                            <span>{item.name}</span>
+                        </div>
+                        ))
+                    }
+
                 </div>
             </section>
-
+            {/* 
             <footer>
-                
+             
                 <button> Projetos  <IoIosArrowDown className="iconButton" style={{}}/></button>
             </footer>
-
+            */}
         </div>
     )
 }
