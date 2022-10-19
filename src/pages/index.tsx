aa79cf69b68ea7fae3e3bd52cbedf0b9f13c5039
Diego Arthur <digo.arthur@hotmail.com>
2022-10-20 00:32:31 +0100

    fix : remove lib react-typical

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import styles from './home.module.scss'
-import Typical from 'react-typical'
 import TypeWriter from 'typewriter-effect'
 export default function Home() {
 
