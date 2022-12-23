92917c7f825d6ed19cb9aaf30456330912291904
Diego Arthur <digo.arthur@hotmail.com>
2022-12-23 23:39:11 +0100

    feat: completed all steps

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -2,64 +2,64 @@
 /* eslint-disable @next/next/no-img-element */
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
-import Timeline from '../../components/Timeline'
 import styles from './styles.module.scss'
 import { useEffect, useState } from 'react'
-import nameSkill from '../../utils/nameSkills'
+import nameSkills from '../../utils/nameSkills'
+import iconsProjects from '../../utils/iconsProjects'
 export default function Projects() {
 
     interface Provider {
         name: string;
-        topics:[];
-        html_url:string;
-        description:string;
-        id:number;
-        
-      }
-    
-  
+        topics: [];
+        html_url: string;
+        description: string;
+        id: number;
+
+    }
+
+
     const [projects, setProjects] = useState<Provider[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/users/digoarthur/repos')
             .then(response => response.json())
-            .then(data => setProjects(data.filter((item:Provider)=>{
+            .then(data => setProjects(data.filter((item: Provider) => {
                 return item.topics.includes('deploy' as never)
-               })))
+            })))
     }, [])
     return (
 
-        <div className={styles.university_Container}>
+        <div className={styles.projects_Container}>
             <Header />
 
             <main className={styles.main_Container}>
                 <div className={styles.main_Content}>
                     <h1>Projetos desevolvidos</h1>
-                    <p>Essa página lista os {projects.length} principais projetos que venho desenvolvendo durante a minha jornada como programador.</p>
+                    <p>Essa página lista os <strong>{projects.length}</strong> principais projetos que venho desenvolvendo durante a minha jornada como programador.</p>
 
                     <div className={styles.card_Container}>
                         {projects.map((element) => {
                             return (
                                 <a key={element.id} href={element.html_url} className={styles.card}>
-                                       {
-                                        element.topics.map((item)=>{
+                                    {
+                                        element.topics.map((item) => {
                                             return (
-                                                item=="javascript"?( <img src={nameSkill[item]}></img>):''
+                                                iconsProjects[item] ? (<img key={item} src={iconsProjects[item]}></img>) : ''
                                             )
                                         })
                                     }
                                     <h2>{element.name}</h2>
                                     <p>{element.description}</p>
                                     <footer className={styles.card_Footer}>
-                                    {
-                                        element.topics.map((item)=>{
-                                            return (
-                                                item=="deploy"?'':(
-                                                <img src={nameSkill[item]}></img>
+                                        {
+                                            element.topics.map((item) => {
+                                                return (
+                                                    item == "deploy" || iconsProjects[item] ? '' : (
+                                                        <img key={item} src={nameSkills[item]}></img>
+                                                    )
                                                 )
-                                            )
-                                        })
-                                    }
+                                            })
+                                        }
                                     </footer>
                                 </a>
                             )
