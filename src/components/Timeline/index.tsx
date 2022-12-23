e303af4a98b18b70ae92289b8446c177514518e6
Diego Arthur <digo.arthur@hotmail.com>
2022-12-24 00:11:17 +0100

    fix: deploy

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,5 +1,5 @@
 
-
+import timelineElements from "./data/index";
 import { FaUniversity } from 'react-icons/fa';
 import { GiMagnifyingGlass, GiTeacher, GiWhiteBook } from 'react-icons/gi';
 import { BiExtension } from 'react-icons/bi';
@@ -16,6 +16,70 @@ export default function Timeline() {
     let bookIconStyles = { background: "#4a2512" }
 
     return (
-       <></>
+        <div className={styles.timelineContainer}>
+            <h1 className={styles.title}>Universidade</h1>
+            <h5 className={styles.subTitle}>Monitorias, Projetos de Extensão, Pesquisas, Livro</h5>
+
+            <h2 className={styles.titleTimeLine}>Timeline</h2>
+            <VerticalTimeline>
+                {
+                    timelineElements.map((element) => {
+
+                        let showButton =
+                            element.buttonText !== undefined &&
+                            element.buttonText !== null &&
+                            element.buttonText !== ""
+
+                        return (
+                            <VerticalTimelineElement
+                                key={element.title}
+                                date={element.date}
+                                dateClassName="date"
+                                iconStyle={
+                                    element.type === 'Formacao' ? formacaoIconStyles
+                                        :
+                                        element.type === 'Pesquisa' ? pesquisaIconStyles
+                                            :
+                                            element.type === 'Monitoria' ? monitoriaIconStyles
+                                                :
+                                                element.type === 'Projeto' ? projetoIconStyles
+                                                    :
+                                                    element.type === 'Livro' ? bookIconStyles
+                                                        :
+                                                        projetoIconStyles
+                                }
+                                icon={
+                                    element.type === 'Formacao' ? <FaUniversity className={styles.icon} />
+                                        :
+                                        element.type === 'Pesquisa' ? <GiMagnifyingGlass className={styles.icon} />
+                                            :
+                                            element.type === 'Monitoria' ? <GiTeacher className={styles.icon} />
+                                                :
+                                                element.type === 'Projeto' ? <BiExtension className={styles.icon} />
+                                                    :
+                                                    element.type === 'Livro' ? <GiWhiteBook className={styles.icon} />
+                                                        :
+                                                        ''
+                                }
+                                className={styles.time}
+
+                            >
+                                <h3 className={styles.verticalTimelineElementTitle}>
+                                    {element.title}
+                                </h3>
+                                <h5 className={styles.verticalTimelineElementSubtitle}>
+                                    {element.location}
+                                </h5>
+                                {element.type === 'Monitoria' || element.type === 'Projeto' ? <p>Linguagem: {element.language} </p> : ``}
+                                <p className={styles.description}>{element.description}</p>
+                                {element.type === 'Monitoria' || element.type === 'Projeto' ? <p>Atividade: {element.activity} </p> : ``}
+                                {element.type === 'Monitoria' || element.type === 'Projeto' ? <p>Orientador: {element.advisor} </p> : ``}
+
+                            </VerticalTimelineElement>
+                        )
+                    })
+                }
+            </VerticalTimeline>
+        </div >
     )
 }
